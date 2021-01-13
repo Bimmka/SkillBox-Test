e154070a07be70d23af5e9abd1233511f191fc82
Bimmka <bibimmka@gmail.com>
2021-01-13 17:35:40 +0400

    Add test

---
+++ Assets/Scripts/Monobehaviour/Static Platform/StaticPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider)), RequireComponent(typeof(Rigidbody))]
+public class StaticPlatform : MonoBehaviour
+{
+    private Rigidbody platformRigidbody;
+
+    private void Awake()
+    {
+        TryGetComponent(out platformRigidbody);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Static Zone")) SetPlatformKinematic();
+    }
+
+    private void SetPlatformKinematic()
+    {
+        platformRigidbody.isKinematic = true;
+    }
+}
